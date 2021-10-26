4ca2cc39187402843bd5cc13dbf31eada0ad821b
unknown <binderyoni@example.com>
2021-10-26 16:39:42 +0300

    test 2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React, { useRef,useState} from 'react';
 import './Header.css';
 import {Link,NavLink} from "react-router-dom";
 import Logo from "../../pictures/bitcoinLogo.png";
-// import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-// import ShoppingCart from './ShoppingCart'
-// import Popover from 'react-bootstrap/Popover'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import ShoppingCart from './ShoppingCart'
+import Popover from 'react-bootstrap/Popover'
 import {useAuth} from '../../context/AuthShopContext'
 
 
@@ -34,7 +34,7 @@ export default function Header (){
                 <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav" >
                     <li>
-                    {/* <OverlayTrigger
+                    <OverlayTrigger
                     trigger={['hover', 'focus']}
                     key="bottom"
                     placement="bottom"
@@ -46,12 +46,12 @@ export default function Header (){
                         </Popover.Content>
                         </Popover>
                     }
-                    > */}
+                    >
                     <Link to="/cart">
                         <div className="navbar-brand">
                     <i className="fas fa-shopping-cart"></i>{cart.length>0 && <span id="numItems" >{cart.length}</span>} 
                     </div></Link>
-                    {/* </OverlayTrigger> */}
+                    </OverlayTrigger>
                     </li>
                     
                     <li className="nav-item">
